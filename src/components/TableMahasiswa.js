52e239420bea34bedfd2e396b4463caab8a92fb7
Frinaldi Muhammad Syauqi <frinaldi.syauqi@gmail.com>
2022-12-21 22:50:19 +0700

    Menambah function lihat data

--- src/components/TableMahasiswa.js
+++ src/components/TableMahasiswa.js
@@ -4,10 +4,31 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function TableMahasiswa() {
-  const API_URL = "http://localhost:5984";
+  const API_URL = "http://localhost:5984/mahasiswas/_find";
+  const username = "admin";
+  const password = "password";
   const [mahasiswa, setMahasiswa] = useState([]);
+  const data = {
+    selector: {
+      _id: {
+        $gt: null,
+      },
+    },
+  };
   useEffect(() => {
-    axios.post(`${API_URL}/mahasiswas/_find`);
+    axios
+      .post(API_URL, data, {
+        headers: {
+          "content-type": "application/json",
+        },
+        auth: {
+          username: username,
+          password: password,
+        },
+      })
+      .then((res) => {
+        setMahasiswa(res.data.docs);
+      });
   }, [mahasiswa]);
   return (
     <Container className="mt-5">
@@ -26,23 +47,25 @@ function TableMahasiswa() {
               </tr>
             </thead>
             <tbody>
+              {mahasiswa.map((items) => (
+                <tr>
+                  <td>{items._id}</td>
+                  <td>{items.nim}</td>
+                  <td>{items.nama}</td>
+                  <td>{items.email}</td>
+                  <td>{items.telp}</td>
+                  <td>
+                    <Button variant="success" size="sm" className="pt-0 m-1">
+                      <FaPen />
+                    </Button>
+                    <Button variant="danger" size="sm" className="pt-0 m-1">
+                      <FaTrashAlt />
+                    </Button>
+                  </td>
+                </tr>
+              ))}
               <tr>
-                <td>1</td>
-                <td>1207050041</td>
-                <td>Sugma</td>
-                <td>Sugma@mail.com</td>
-                <td>08951092123</td>
-                <td>
-                  <Button variant="success" size="sm" className="pt-0 m-1">
-                    <FaPen />
-                  </Button>
-                  <Button variant="danger" size="sm" className="pt-0 m-1">
-                    <FaTrashAlt />
-                  </Button>
-                </td>
-              </tr>
-              <tr>
-                <td>2</td>
+                <td></td>
                 <td>
                   <Form.Control type="text" />
                 </td>
